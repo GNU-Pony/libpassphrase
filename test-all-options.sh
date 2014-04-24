e1a858eb485aa8e5826348ffb75de05061c1e7ff
Mattias Andrée <maandree@operamail.com>
2014-04-24 08:34:27 +0200

    add script that tests all 2K option combinations (including redundant)
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ test-all-options.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for a in PASSPHRASE_ECHO ""; do
+    for b in PASSPHRASE_STAR ""; do
+	for c in PASSPHRASE_REALLOC ""; do
+	    for d in PASSPHRASE_MOVE ""; do
+		for e in PASSPHRASE_INSERT ""; do
+		    for f in PASSPHRASE_OVERRIDE ""; do
+			for g in PASSPHRASE_DELETE ""; do
+			    for h in PASSPHRASE_CONTROL ""; do
+				for i in PASSPHRASE_DEDICATED ""; do
+				    for j in DEFAULT_INSERT ""; do
+					for k in PASSPHRASE_INVALID ""; do
+					    make libpassphrase -B OPTIONS="$a $b $c $d $e $f $g $h $i $j $k" || exit 1
+					done
+				    done
+				done
+			    done
+			done
+		    done
+		done
+	    done
+	done
+    done
+done
+
