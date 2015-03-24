6abbdab8c115b87c6374f0617337e999c03e5a88
Mattias Andrée <maandree@operamail.com>
2015-03-24 16:00:51 +0100

    update test-all-options.sh
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- test-all-options.sh
+++ test-all-options.sh
@@ -2,16 +2,18 @@
 
 for a in PASSPHRASE_ECHO ""; do
     for b in PASSPHRASE_STAR ""; do
-	for c in PASSPHRASE_REALLOC ""; do
-	    for d in PASSPHRASE_MOVE ""; do
-		for e in PASSPHRASE_INSERT ""; do
-		    for f in PASSPHRASE_OVERRIDE ""; do
-			for g in PASSPHRASE_DELETE ""; do
-			    for h in PASSPHRASE_CONTROL ""; do
-				for i in PASSPHRASE_DEDICATED ""; do
-				    for j in DEFAULT_INSERT ""; do
-					for k in PASSPHRASE_INVALID ""; do
-					    make libpassphrase -B OPTIONS="$a $b $c $d $e $f $g $h $i $j $k" || exit 1
+	for c in PASSPHRASE_TEXT ""; do
+	    for d in PASSPHRASE_REALLOC ""; do
+		for e in PASSPHRASE_MOVE ""; do
+		    for f in PASSPHRASE_INSERT ""; do
+			for g in PASSPHRASE_OVERRIDE ""; do
+			    for h in PASSPHRASE_DELETE ""; do
+				for i in PASSPHRASE_CONTROL ""; do
+				    for j in PASSPHRASE_DEDICATED ""; do
+					for k in DEFAULT_INSERT ""; do
+					    for l in PASSPHRASE_INVALID ""; do
+						make libpassphrase -B OPTIONS="$a $b $c $d $e $f $g $h $i $j $k $l" || exit 1
+					    done
 					done
 				    done
 				done
